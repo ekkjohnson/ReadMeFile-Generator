2948e80ead5d501fda5fcfe65362c4e85f3b9afe
Elizabeth <ekkjohnson@gmail.com>
2022-04-12 18:27:58 -0500

    questions

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ const fs = require('fs');
 const path = require('path');
 const inquirer = require('inquirer');
 const generateMarkdown = require('./generateMarkdown');
+console.log("welcome to my readMe generator, answer the following questions to create a README.md")
 // TODO: Create an array of questions for user input
-const questions = () => {
-inquirer
-  .prompt([
+const questions =[ 
+// inquirer
+//   .prompt([
     {
         type: "input",
         name: "name",
@@ -33,11 +34,12 @@ inquirer
         message: "Please write a short description of your project",
     },
     {
-        type: "input",
-        name: "license",
-        message: "What licsense are you using?",
+        type: 'list',
+        message: "Choose a license for your project.",
+        choices: ['GNU AGPLv3', 'GNU GPLv3', 'GNU LGPLv3', 'Mozilla Public License 2.0', 'Apache License 2.0', 'MIT License', 'Boost Software License 1.0', 'The Unlicense'],
+        name: 'license',
     },
-])
+]
 
 .then((data) => {
     const generateMarkdown= generateMarkdown(data);
